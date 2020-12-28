8f2dda047f8a125b47c714191b5a18f7dc10dfd6
gustrow <gustavo.ruiz@gmail.com>
2020-12-27 21:09:53 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
   file { '/root/puppettest1.txt':
     ensure => 'file',
-    content => 'hello world',
+    content => 'hello world (test)',
     owner => 'root',
     group => 'root',
     mode => '0640',
